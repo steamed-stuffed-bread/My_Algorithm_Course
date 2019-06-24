276b851ee9de278415aae812ca308881af50c246
刘赛 <liusai@s-MacBook-Pro.local>
2019-06-24 22:57:01 +0800

    Course2 week1

--- Course2/Stanford_Ass_5.py
+++ Course2/Stanford_Ass_5.py
@@ -5,6 +5,7 @@ class node:
         self.id = _id
         self.edges = edges or []
         self.explored = False
+        self.finished = False
         self.finish = 0
 
 class graph:
@@ -12,9 +13,9 @@ class graph:
         self.G = {}
 
     def original(self):
-        f = open("test.txt")
+        f = open("SCC.txt")
         for line in f.readlines():
-            (_id, out) = (int(n) for n in line.split(" "))
+            (_id, out) = (int(n) for n in line.split(' ',1))
             if _id not in self.G:
                 self.G[_id] = node(_id, edges = [out])
             else:
@@ -29,42 +30,49 @@ class graph:
         for node in self.G.values():
             node.edges = [-edge for edge in node.edges]
             node.explored = False
+            node.finished = False
+
+global res
+res = []
 
 def dfs(G, node):
     exp = 0
     stack = [node]
     while stack:
-        node = stack.pop()
+        node = stack[-1]
         if not node.explored:
             node.explored = True
             exp = exp + 1
             for edge in node.edges:
                 if edge > 0 and not G[edge].explored:
                     stack.append(G[edge])
+        else:
+            if not node.finished:
+                node.finished = True
+                res.append(stack.pop())
+            else:
+                stack.pop()
     return exp
 
 def dfs_loop(node_order):
-    res = []
+    exo = []
     for node in node_order:
         if not node.explored:
-            print "Exploring the graph from node %s" % node.id
             t = dfs(G, node)
-            node.finish = t
-            res.append(t)
-    return res
+            exo.append(t)
+    return exo
 
 if __name__ == "__main__":
     c = graph()
     c.original()
     c.reverse()
     G = c.G
-    res = dfs_loop(sorted(G.values(),
+    dfs_loop(sorted(G.values(),
                     key=lambda x:x.id,
                     reverse=True))
-    print res
-    #c.reverse()
-    #G = c.G
-    #result = dfs_loop(sorted(G.values(),
-    #                  key=lambda x:x.finish,
-    #                  reverse=True))
-    #print result
+    finish_time = reversed(res)
+    c.reverse()
+    G = c.G
+    result = dfs_loop(finish_time)
+    result.extend([0]*5)
+    print sorted(result, reverse=True)[:5]
